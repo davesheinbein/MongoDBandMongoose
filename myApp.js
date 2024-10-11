0b0e3c975c52b4baf0f00e6a8eda4406cd31d93f
David Sheinbein <davidsheinbeindev@gmail.com>
2024-10-10 17:22:06 -0700

    Solution Update

--- myApp.js
+++ myApp.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-console.log('MONGO_URI:', process.env.MONGO_URI);
-
 mongoose
 	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
